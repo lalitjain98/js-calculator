d0d1ae57addc3243ccbe9afb1627a300011df2a4
Lalit Jain <jain.lalit138@gmail.com>
2019-04-16 18:06:03 +0530

    Updates

---
+++ precache-manifest.340719d40173ad9a501b95169432e9d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/js-calculator/static/media/digital-7.1e670d88.ttf"
+  },
+  {
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/js-calculator/static/media/digital-7 (mono).58045dab.ttf"
+  },
+  {
+    "revision": "03c2fbc48320b8ae5e5f",
+    "url": "/js-calculator/static/js/runtime~main.9377886e.js"
+  },
+  {
+    "revision": "640e931bd33758e581aa",
+    "url": "/js-calculator/static/js/main.60f77779.chunk.js"
+  },
+  {
+    "revision": "c80b59e04892e64170ef",
+    "url": "/js-calculator/static/js/2.a6e72ab3.chunk.js"
+  },
+  {
+    "revision": "640e931bd33758e581aa",
+    "url": "/js-calculator/static/css/main.fb1d6c66.chunk.css"
+  },
+  {
+    "revision": "31ed5687509090ed0dbc9a83cea09efe",
+    "url": "/js-calculator/index.html"
+  }
+];
